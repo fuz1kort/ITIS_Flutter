e320a0ddbcf7310307be3528cbd1b27439373eea
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2025-05-12 12:17:13 +0300

    empty

--- Hw5/Mobile5.Backend/Mobile5.Backend/obj/Debug/net9.0/Mobile5.Backend.AssemblyInfo.cs
+++ Hw5/Mobile5.Backend/Mobile5.Backend/obj/Debug/net9.0/Mobile5.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mobile5.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c2badf8b6cc545e1830987edcf333585856f4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e59dbc40926fa204c55b2f72c7b3e5ea76d48323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mobile5.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mobile5.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
